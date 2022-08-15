6b68732486314c3f2ad71a9423a81d9a27e07cee
Matthew Watson <Me@MatthewTheDeveloper.com>
2022-08-15 09:20:08 -0600

    - added firebase config to prod environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBsy03qoAlCIgVWTIAgFpETXrCcIdNwK-4",
+    authDomain: "file-attachment-57ac0.firebaseapp.com",
+    projectId: "file-attachment-57ac0",
+    storageBucket: "file-attachment-57ac0.appspot.com",
+    messagingSenderId: "4632517516",
+    appId: "1:4632517516:web:ec900dad10a07a55a614a9"
+  }
 };
