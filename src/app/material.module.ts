6ac9919c60527cb79e1f6a7cf7966fdc4ecf8d9b
Matthew Watson <Me@MatthewTheDeveloper.com>
2022-10-31 10:58:06 -0400

    - I don't know why these name are so ungodly long

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -20,6 +21,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatDatepickerModule,
     MatDialogModule,
     MatDividerModule,
     DragDropModule,
